10e4428a4161de23debc822e1ece9073c9018095
lukeber4 <lukeber4@gmail.com>
2016-09-30 12:42:05 -0300

    Update columns_maxtomin.sh

--- columns_maxtomin.sh
+++ columns_maxtomin.sh
@@ -5,4 +5,4 @@ file=$1
 max=`cat $file | awk '{print NF}' | sort -n | uniq -c | sort -n | awk '{print $2}' | head -1`
 min=`cat $file | awk '{print NF}' | sort -n | uniq -c | sort -n | awk '{print $2}' | tail -1`
 
-for i in $(seq $max $min); do cat $file | awk 'NF=='"$i"'{print}{}'; done > $file.new
+for ((i=$max; i>=$min; i--)); do cat $file | awk 'NF=='"$i"'{print}{}'; done > $file.new
